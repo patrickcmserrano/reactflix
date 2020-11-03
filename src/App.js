3c23e7593c4ea2b34760db19bbf605a38bb54b87
Patrick Serrano <patrickcmserrano@gmail.com>
2020-11-03 20:24:42 -0300

    movierow components with props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow.js'
 
 export default () => {
+
+    const [movieList, setMovieList] = useState([]);
 
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
-            console.log(list);
+            setMovieList(list);
         }
 
         loadAll();
     }, []);
 
     return (
-        <div>
-            Olá Mundo!
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     )
 }
---
+++ src/components/MovieRow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MovieRow.css'
+
+export default ({items, title}) => {
+    return (
+        <div>
+            <h2>
+                {title}
+            </h2>
+
+        </div>
+    )
+}
