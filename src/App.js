e34788ab9cd11e76021f78af7dc1d3398802c33b
Patrick Serrano <patrickcmserrano@gmail.com>
2020-11-05 08:55:24 -0300

    background-color + font-family

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow.js'
+import "./App.css";
 
 export default () => {
 
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -4,10 +4,12 @@ import './MovieRow.css'
 export default ({items, title}) => {
     return (
         <div>
-            <h2>
-                {title}
-            </h2>
-
+            <h2>{title}</h2>
+            <div className={'movieRow--listarea'}>
+                {items.results.length > 0 && items.results.map((item, key) => (
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={'item.original_title'}/>
+                ))}
+            </div>
         </div>
     )
 }
